31934936fbefee303aed87fc650f01a67510084f
zhulixin <zhulx521@gmail.com>
2012-12-19 22:24:14 +0800

    add attribute

--- magics.py
+++ magics.py
@@ -203,3 +203,36 @@ cls = MyClass()
 # 可以在类的实例上调用静态方法和类方法
 cls.smeth()         # This is a static method
 cls.cmeth()         # This is a class method of <class '__main__.MyClass'>
+
+
+# __getattribute__(self, name):当特性name被访问时自动被调用（只能在新式类中使用）
+# __getattr__(self, name)：当特性name被访问且对象没有相应的特性时被自动调用
+# __setattr__(self, name, value)：当试图给特性name赋值时会被自动调用.
+# __delattr__(self, name)：当试图删除特性name时被自动调用
+class Rectangle:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+
+    def __setattr__(self, key, value):
+        print 'setattr', key
+        if key == 'size':
+            self.width, self.height = value
+        else:
+            self.__dict__[key] = value
+
+    def __getattr__(self, item):
+        print 'getattr'
+        if item == 'size':
+            return self.width, self.height
+        else:
+            raise AttributeError
+# setattr width
+# setattr height
+rt = Rectangle()
+# setattr size
+# setattr width
+# setattr height
+rt.size = 6, 9
+# getattr
+print rt.size    # (6, 9)
