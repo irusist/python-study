5fd1b8f76e3838a09eca7bf0962e62000df2b5ba
zhulx <zhulx521@gmail.com>
2013-02-04 10:25:25 +0800

    add edxtension

---
+++ extension.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+__author__ = 'zhulixin'
+
+# Psyco(http://psyco.sourceforge.net/)
+# Pyrex(http://www.cosc.canterbury.ac.nz/greg.ewing/python/Pyrex/)
+# PyPy(http://pypy.org/)
+# Weave(http://www.scipy.org/Weave)
+# NumPy(http://www.numpy.org/)
+# ctypes模块(http://docs.python.org/2/library/ctypes.html)
+# Subprocess模块(http://docs.python.org/2/library/subprocess.html)
+# PyCXX(http://cxx.sourceforge.net/)
+# SIP(http://www.riverbankcomputing.com/software/sip/intro)
+# Boost.Python(http://www.boost.org/doc/libs/1_52_0/libs/python/doc/)
+# Modulator(http://svn.python.org/projects/!svn/bc/5000/python/trunk/Tools/modulator/modulator.py)
+# SWIG(http://www.swig.org)
+
+
+# 引数计数器     http://docs.python.org/3/extending/extending.html#reference-counts
+# 通过gc模块可以访问python垃圾收集器     http://docs.python.org/3/library/gc.html
+# http://docs.python.org/3/c-api/arg.html
